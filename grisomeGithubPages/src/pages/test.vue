0fe64c8004d38cf3cd8b48a73269e82a7592111b
Juan Jose Castro Cruz <jcastro@cafetosoftware.com>
2021-05-18 23:12:09 -0500

    First commit with test, without config githubpages

---
+++ grisomeGithubPages/src/pages/test.vue
@@ -0,0 +1,18 @@
+<template>
+  <bloglayout>
+    <h1>Test</h1>
+    <p>Hola soy Juanjo</p>
+  </bloglayout>
+</template>
+
+<script>
+import bloglayout from "~/layouts/blog";
+export default {
+  components: {
+    bloglayout,
+  },
+  metaInfo: {
+    title: "test",
+  },
+};
+</script>
